9570b5b69a35ab5d676d2fefac5a486fa9ca53ff
Michael Kawata <michaelkawata@bens-air.lan>
2022-12-06 12:58:20 -0800

    added fetch to send data to a server

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-async function main(){
+async function main() {
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
@@ -6,17 +6,36 @@ async function main(){
     const response = await fetch('http://localhost:9001/counter');
 
     const result = await response.json();
-    
+
     let countValue = result.value;
 
-    function increment(){
+    function increment() {
         countValue++;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
-    function decrement(){
+    function decrement() {
         countValue--;
         countContainer.textContent = countValue;
+        fetch('http://localhost:9001/counter', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
